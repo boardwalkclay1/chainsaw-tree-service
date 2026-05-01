94b49a19e9974039ceace87baaa98963ab4ae1eb
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-30 20:48:14 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-import hero from "./modules/hero/hero.js";
-import difference from "./modules/difference/difference.js";
-import estimate from "./modules/estimate/estimate.js";
-import selfCheck from "./modules/selfCheck/selfCheck.js";
-import services from "./modules/services/services.js";
-import gardening from "./modules/gardening/gardening.js";
-import about from "./modules/about/about.js";
-import footer from "./modules/footer/footer.js";
+import hero from "./modules/hero.js";
+import difference from "./modules/difference.js";
+import estimate from "./modules/estimate.js";
+import selfCheck from "./modules/selfCheck.js";
+import services from "./modules/services.js";
+import gardening from "./modules/gardening.js";
+import about from "./modules/about.js";
+import footer from "./modules/footer.js";
+import yardDesigner from "./tools/yardDesigner.js";
+import initScroll from "./logic/scroll.js";
+import initSeasonal from "./logic/seasonal.js";
 
 const app = document.getElementById("app");
 
@@ -17,4 +20,8 @@ app.innerHTML =
   services() +
   gardening() +
   about() +
+  yardDesigner() +
   footer();
+
+initScroll();
+initSeasonal();
