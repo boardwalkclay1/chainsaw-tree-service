724feaa06ba29f969e81238bcfb2db86c3e2f0cb
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-07-05 13:13:50 -0400

    Delete public/climbing-class/functions/admin-students.js

--- public/climbing-class/functions/admin-students.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(req, env) {
-    const { results } = await env.DB.prepare(
-      "SELECT id, name, email FROM students ORDER BY created_at DESC"
-    ).all();
-
-    return Response.json(results);
-  }
-};
