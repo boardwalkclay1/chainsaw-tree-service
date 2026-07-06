579e473d19b5e813d4663287788eb78b96d3e5a8
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-07-05 22:27:23 -0400

    Delete public/climbing-class/functions directory

--- public/climbing-class/functions/classes.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(req, env) {
-    const { results } = await env.DB.prepare(
-      "SELECT * FROM classes ORDER BY date ASC"
-    ).all();
-
-    return Response.json(results);
-  }
-};
