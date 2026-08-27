d6c57d3e1dba4ab684bea44e92f3adb23a56968e
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-08-27 01:31:53 -0400

    Create cutting.js

---
+++ public/climbing-class/admin/js/modules/cutting.js
@@ -0,0 +1,21 @@
+window.CUTTING = {
+  title: "Cutting Techniques",
+  subcategories: {
+    basics: {
+      title: "Basic Cuts",
+      videos: []
+    },
+    advanced: {
+      title: "Advanced Cuts",
+      videos: []
+    },
+    riggingCuts: {
+      title: "Rigging Cuts",
+      videos: []
+    },
+    chainsawSafety: {
+      title: "Chainsaw Safety",
+      videos: []
+    }
+  }
+};
