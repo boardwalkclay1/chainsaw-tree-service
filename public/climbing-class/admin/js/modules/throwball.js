3760bb665f1d99dde61e3c71d982cfc5d4ad9351
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-08-27 01:47:05 -0400

    Update throwball.js

--- public/climbing-class/admin/js/modules/throwball.js
+++ public/climbing-class/admin/js/modules/throwball.js
@@ -1,18 +1,36 @@
 window.THROWBALL = {
   title: "Throwball",
+
   subcategories: {
     intro: {
       title: "Intro Lessons",
-      videos: []
+      videos: [
+        {
+          title: "Intro Lesson (Part 2)",
+          src: "https://github.com/boardwalkclay1/throwball/releases/download/throwball/TRIAL____________throwball-intro.mp4"
+        },
+        {
+          title: "Intro Lesson (Part 1)",
+          src: "https://github.com/boardwalkclay1/throwball/releases/download/throwball-2/throwball-intro.mp4"
+        }
+      ]
     },
+
     anchors: {
       title: "Anchor Setting",
-      videos: []
+      videos: [
+        {
+          title: "Second Lesson",
+          src: "https://github.com/boardwalkclay1/throwball/releases/download/throwball-2/Thrwball-part-3.mp4"
+        }
+      ]
     },
+
     advanced: {
       title: "Advanced Throwline Techniques",
       videos: []
     },
+
     recovery: {
       title: "Throwline Recovery",
       videos: []
