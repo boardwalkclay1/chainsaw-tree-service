492de4e564ad6fbe1cb6802270a2540748088087
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-08-27 01:31:36 -0400

    Create throwball.js

---
+++ public/climbing-class/admin/js/modules/throwball.js
@@ -0,0 +1,21 @@
+window.THROWBALL = {
+  title: "Throwball",
+  subcategories: {
+    intro: {
+      title: "Intro Lessons",
+      videos: []
+    },
+    anchors: {
+      title: "Anchor Setting",
+      videos: []
+    },
+    advanced: {
+      title: "Advanced Throwline Techniques",
+      videos: []
+    },
+    recovery: {
+      title: "Throwline Recovery",
+      videos: []
+    }
+  }
+};
