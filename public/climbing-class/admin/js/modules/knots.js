ba3d93d8d134a571b14b78df2739f0fd9deceb67
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-08-27 13:32:57 -0400

    Update knots.js

--- public/climbing-class/admin/js/modules/knots.js
+++ public/climbing-class/admin/js/modules/knots.js
@@ -1,31 +1,15 @@
-window.KNOTS = {
+export const KNOTS = {
   title: "Knots",
+
   subcategories: {
-    bowline: {
-      title: "Bowline",
-      videos: [
-        {
-          title: "Running Bowline — Uses & Scenarios",
-          src: "/videos/knots/running-bowline.mp4",
-          tags: "bowline running knots"
-        },
-        {
-          title: "Bowline on a Bight",
-          src: "/videos/knots/bowline-on-a-bight.mp4",
-          tags: "bowline bight knots"
-        }
-      ]
+    basics: {
+      title: "Basic Knots",
+      videos: []
     },
 
-    frictionHitches: {
-      title: "Friction Hitches",
-      videos: [
-        {
-          title: "Blake’s Hitch",
-          src: "/videos/knots/blakes-hitch.mp4",
-          tags: "blakes hitch knots"
-        }
-      ]
+    climbingKnots: {
+      title: "Climbing Knots",
+      videos: []
     },
 
     riggingKnots: {
