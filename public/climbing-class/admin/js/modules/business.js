e5f159cccd98f2fcb96c961b9424cc2b3bdc2b74
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-08-27 13:25:59 -0400

    Update business.js

--- public/climbing-class/admin/js/modules/business.js
+++ public/climbing-class/admin/js/modules/business.js
@@ -1,18 +1,22 @@
-window.BUSINESS = {
+export const BUSINESS = {
   title: "Business & Sales",
+
   subcategories: {
     pricing: {
       title: "Pricing Jobs",
       videos: []
     },
+
     communication: {
       title: "Customer Communication",
       videos: []
     },
+
     sales: {
       title: "Sales Techniques",
       videos: []
     },
+
     liability: {
       title: "Liability & Safety",
       videos: []
