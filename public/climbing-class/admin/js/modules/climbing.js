579d059b55f2ca05c10e870ca4bf156143265bc5
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-08-27 01:31:19 -0400

    Add climbing techniques module with subcategories

---
+++ public/climbing-class/admin/js/modules/climbing.js
@@ -0,0 +1,21 @@
+window.CLIMBING = {
+  title: "Climbing Techniques",
+  subcategories: {
+    srt: {
+      title: "SRT",
+      videos: []
+    },
+    drt: {
+      title: "DRT",
+      videos: []
+    },
+    footlock: {
+      title: "Footlocking",
+      videos: []
+    },
+    movement: {
+      title: "Movement & Positioning",
+      videos: []
+    }
+  }
+};
